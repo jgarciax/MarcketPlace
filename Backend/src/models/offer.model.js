b0362515b20878d494f5e59afd9168a30e07a4f2
Jared-Js <Edsonpatzan@gmail.com>
2023-12-04 00:00:56 -0600

    Models

--- Backend/src/models/offer.model.js
+++ Backend/src/models/offer.model.js
@@ -3,6 +3,38 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const offerSchema = new Schema{
-    
-}
+const offerSchema = new Schema({
+    seller: {
+        type: Schema.Types.ObjectId,
+        require: true,
+        ref: 'user'
+    }, 
+    buyer: {
+        type: Schema.Types.ObjectId,
+        require: true,
+        ref: 'user'
+    },
+    model: {
+        type: String,
+        require: true
+    },
+    marca: {
+        type: String,
+        require: true
+    },
+    estate:{
+        type: String,
+        require: true
+    },
+    price:{
+        type: Number,
+        require: true
+    },
+    description: {
+        type: String,
+        require: true
+    },
+    date: {
+        type: date,default:Date.now
+    }
+})
